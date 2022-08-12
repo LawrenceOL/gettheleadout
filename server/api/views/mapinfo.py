82383bbf62ff68c57eda1ace777150e33c9dbf2f
Timothy Moon <timhgmoon@gmail.com>
2022-08-11 21:49:08 -0700

    allow get/update/delete by id

--- server/api/views/mapinfo.py
+++ server/api/views/mapinfo.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 from ..serializers.mapinfo import MapSerializer
 from ..serializers.mapinfo import MapInfo
 
-class MapsView(generics.ListCreateAPIView):
+class MapsList(generics.ListCreateAPIView):
   queryset = MapInfo.objects.all()
   serializer_class = MapSerializer
+
+class MapDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = MapInfo.objects.all()
+  serializer_class = MapSerializer
+  lookup_field = "id"
