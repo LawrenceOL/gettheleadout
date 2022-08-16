2a5ef9eb0999a1939e9ab37cd706982d2a1cb366
Timothy Moon <timhgmoon@gmail.com>
2022-08-16 16:53:02 -0700

    changed model

--- server/api/models/mapinfo.py
+++ server/api/models/mapinfo.py
@@ -1,21 +1,18 @@
 from django.db import models
 
 class MapInfo(models.Model):
-  #used for suspected_lead
-  PROBABILITY_OF_LEAD_CHOICES = [
+  LEAD_INFO_CHOICES = [
+    ('CONFIRMED NON-LEAD', 'Confirmed Non-Lead'),
     ('ASSUMED NON-LEAD', 'Assumed Non-Lead'),
-    ('UNLIKELY', 'Unlikely Lead'),
-    ('MAYBE', 'Maybe Lead'),
-    ('LIKELY', 'Likely Lead'),
+    ('CONFIRMED LEAD', 'Confirmed Lead'),
     ('ASSUMED LEAD', 'Assumed Lead'),
-    ('NON-RES', 'Non-residential'),
-    ('UNKNOWN', 'Unknown')
+    ('NON-RES', "Non-Residential"),
+    ('NO INFO', 'No Information')
   ]
   latitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
   longitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
   address = models.CharField(max_length=50)
-  confirmed_lead = models.BooleanField(default=False)
-  suspected_lead = models.CharField(max_length=20, choices=PROBABILITY_OF_LEAD_CHOICES, default='UNKNOWN')
+  lead_options = models.CharField(max_length=20, choices=LEAD_INFO_CHOICES, default='NO INFO')
 
   def __str__(self):
-    return f"{self.latitude}, {self.longitude}, {self.address}, {self.confirmed_lead}, {self.suspected_lead}"
+    return f"{self.latitude}, {self.longitude}, {self.address}, {self.lead_options}"
--- server/api/serializers/mapinfo.py
+++ server/api/serializers/mapinfo.py
@@ -4,4 +4,4 @@ from ..models.mapinfo import MapInfo
 class MapSerializer(serializers.ModelSerializer):
   class Meta:
     model = MapInfo
-    fields = ('id', 'address', 'latitude', 'longitude', 'confirmed_lead', 'suspected_lead')
+    fields = ('id', 'address', 'latitude', 'longitude', 'lead_options')
