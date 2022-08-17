4ec363d13f4c683eb9347b9a0ab073a021f2801c
Timothy Moon <timhgmoon@gmail.com>
2022-08-16 19:56:25 -0700

    model we'd use if we were to import csv file into db

--- server/api/models/mapinfo.py
+++ server/api/models/mapinfo.py
@@ -1,18 +1,34 @@
 from django.db import models
 
 class MapInfo(models.Model):
-  LEAD_INFO_CHOICES = [
-    ('CONFIRMED NON-LEAD', 'Confirmed Non-Lead'),
-    ('ASSUMED NON-LEAD', 'Assumed Non-Lead'),
-    ('CONFIRMED LEAD', 'Confirmed Lead'),
-    ('ASSUMED LEAD', 'Assumed Lead'),
-    ('NON-RES', "Non-Residential"),
-    ('NO INFO', 'No Information')
-  ]
-  latitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
-  longitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
-  address = models.CharField(max_length=50)
-  lead_options = models.CharField(max_length=20, choices=LEAD_INFO_CHOICES, default='NO INFO')
+  # LEAD_INFO_CHOICES = [
+  #   ('CONFIRMED NON-LEAD', 'Confirmed Non-Lead'),
+  #   ('ASSUMED NON-LEAD', 'Assumed Non-Lead'),
+  #   ('CONFIRMED LEAD', 'Confirmed Lead'),
+  #   ('ASSUMED LEAD', 'Assumed Lead'),
+  #   ('NON-RES', "Non-Residential"),
+  #   ('NO INFO', 'No Information')
+  # ]
+  # latitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
+  # longitude = models.DecimalField(max_digits=25, decimal_places=6, blank=True, null=True)
+  # address = models.CharField(max_length=50)
+  # lead_options = models.CharField(max_length=20, choices=LEAD_INFO_CHOICES, default='NO INFO')
+  pin = models.BigIntegerField()
+  property_class = models.SmallIntegerField()
+  neighborhood_code = models.SmallIntegerField()
+  town_code = models.SmallIntegerField()
+  type_of_residence = models.FloatField()
+  apartments = models.FloatField()
+  longitude = models.FloatField()
+  latitude = models.FloatField()
+  census_tract = models.FloatField()
+  property_address = models.CharField(max_length=255)
+  age = models.SmallIntegerField()
+  neighbor_mapping = models.SmallIntegerField()
+  town_and_neighbor = models.IntegerField()
+  after_ban = models.BooleanField(null=True, blank=True)
+  
+
 
   def __str__(self):
-    return f"{self.latitude}, {self.longitude}, {self.address}, {self.lead_options}"
+    return f"{self.latitude}, {self.longitude}"
