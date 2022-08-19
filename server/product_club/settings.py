4e26918546c00b77ce8888a24ad8b7e4877ab0d7
Timothy Moon <timhgmoon@gmail.com>
2022-08-19 16:27:49 -0700

    added requirements.txt

--- server/product_club/settings.py
+++ server/product_club/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.environ['DBNAME'],
         'HOST': os.environ['DBHOST'],
         'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'],
+        'PASSWORD': os.environ['DBPASS'] 
     }
 }
 
