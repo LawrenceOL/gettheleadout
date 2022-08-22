ddfa3dee931721a5b46c41a401f310fec33d0aaa
Timothy Moon <timhgmoon@gmail.com>
2022-08-22 13:03:33 -0700

    load/mark lead pipe locations

--- client/src/Components/Main/LandingPage/Map.js
+++ client/src/Components/Main/LandingPage/Map.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { MapContainer, TileLayer, Marker, ZoomControl } from "react-leaflet";
 import "./Map.css"
+const axios = require('axios').default;
 
 const Map = () => {
+    const [leadData, setLeadData] = useState([])
+    
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/map/')
+        .then(function(res) {
+            setLeadData(res.data)
+            console.log(res.data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    }, [])
+
     return (
         <>
       <div className="mapholder">
@@ -12,17 +26,21 @@ const Map = () => {
           </button> */}
           
             <MapContainer
-              center={[38.8977, -77.0365]}
+              center={[41.5833827612176, -87.6711596591997]}
               zoom={17}
-              zoomControl={false}
-              scrollWheelZoom={false}
+              zoomControl={true}
+              scrollWheelZoom={true}
             >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <ZoomControl position="bottomleft" />
-              <Marker position={[38.8977, -77.0365]}></Marker>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                 <ZoomControl position="bottomleft" />
+                <Marker position={[41.5833827612176, -87.6711596591997]}></Marker>
+                {
+                leadData.map((data) => (
+                    <Marker position = {[data.latitude, data.longitude]}></Marker>
+                ))}
             </MapContainer>
 
             {/* <a href="www.lawrenceol.dev">www.gettheleadout.com</a> */}
