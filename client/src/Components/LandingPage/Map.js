03234b086714c144228642001bc09affe039ff52
Lawrence <loreganlloyd@gmail.com>
2022-08-24 09:54:57 -0400

    merging

--- client/src/Components/LandingPage/Map.js
+++ client/src/Components/LandingPage/Map.js
@@ -6,31 +6,44 @@ import "./Map.css";
 const axios = require('axios').default;
 const Map = () => {
     const [leadData, setLeadData] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    // useEffect(() => {
+    //     axios.get('https://leadpipe-api.azurewebsites.net/map/')
+    //     .then(function(res) {
+    //         setLeadData(res.data)
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //     });
+    // }, [])
     
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/map/')
-        .then(function(res) {
-            setLeadData(res.data)
-        })
-        .catch(function (err) {
-            console.log(err);
+      const fetchData = async() => {
+        const res = await axios.get('https://leadpipe-api.azurewebsites.net/map/')
+  
+        setLeadData(res.data)
+      }
+      fetchData()
+        .catch(console.error)
+        .finally(() => {
+          setLoading(false)
         });
     }, [])
     
-
+    
     //Custom Icons for cluster icons
     const customIcon = new L.Icon({
         iconUrl: require("./location.svg").default,
         iconSize: new L.Point(30, 35)
       });
     
-      const createClusterCustomIcon = function (cluster: MarkerCluster) {
-        return L.divIcon({
-          html: `<span>${cluster.getChildCount()}</span>`,
-          className: "custom-marker-cluster",
-          iconSize: L.point(33, 33, true)
-        });
-      };
+      // const createClusterCustomIcon = function (cluster: MarkerCluster) {
+      //   return L.divIcon({
+      //     html: `<span>${cluster.getChildCount()}</span>`,
+      //     className: "custom-marker-cluster",
+      //     iconSize: L.point(33, 33, true)
+      //   });
+      // };
     return (
         <>
       <div className="mapholder">
@@ -48,20 +61,22 @@ const Map = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                 <MarkerClusterGroup 
+                 {isLoading ? 
+                 (<div>Loading..</div>):
+                 (<MarkerClusterGroup 
                     chunkedLoading
-                    onClick={(e) => console.log("onClick", e)}
-                    iconCreateFunction={createClusterCustomIcon}
-                    maxClusterRadius={150}
-                    spiderfyOnMaxZoom={true}
-                    polygonOptions={{
-                      fillColor: "#ffffff",
-                      color: "#ffffff",
-                      weight: 5,
-                      opacity: 1,
-                      fillOpacity: 0.8
-                    }}
-                    showCoverageOnHover={true}
+                    // onClick={(e) => console.log("onClick", e)}
+                    // iconCreateFunction={createClusterCustomIcon}
+                    // maxClusterRadius={150}
+                    // spiderfyOnMaxZoom={true}
+                    // polygonOptions={{
+                    //   fillColor: "#ffffff",
+                    //   color: "#ffffff",
+                    //   weight: 5,
+                    //   opacity: 1,
+                    //   fillOpacity: 0.8
+                    // }}
+                    // showCoverageOnHover={true}
                   >
                   
                     {leadData.map((data) => (
@@ -73,6 +88,7 @@ const Map = () => {
                         ></Marker>
                     ))}
                  </MarkerClusterGroup>
+                 )}
             </MapContainer>
       </div>
       </>
