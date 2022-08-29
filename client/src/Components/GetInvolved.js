1087302d43e2e3873c6aef7cdcfcf8f4d31a848d
veenaprakash123 <veenaprakash2018@gmail.com>
2022-08-29 12:45:42 -0400

    Updated images and font in How to check for lead pipes page

--- client/src/Components/GetInvolved.js
+++ client/src/Components/GetInvolved.js
@@ -14,55 +14,58 @@ const GetInvolved = () => {
   return (
     <div className="topmargin">
       <div className="page-template">
-        <div topmargin="20px">
-          <h1 className="title" style={{fontWeight: 'bold'}}>
-            How can you get involved?
-          </h1>
-        </div>
-
-        <div>
-          <h1 className="supporting-text">
-            {" "}
-            Get involved to help your community progress towards a lead-free
-            future.
-          </h1>
-        </div>
-
-        <br />
-
-        <div>
-          <div className="numbered-sections">
-              <div className="item-1">
-                <img
-                  alt="Number one"
-                  src={One}
-                  height={50}
-                  width={50}
-                />
-              </div>
-              <div className="item-2">
-                  <h1 className="feature-text-top">
-                    {" "}
-                    Stay up-to-date on the impactlead pipes have in your community.
-                  </h1>
-                  <h1 className="feature-text-bottom">
-                    Read through this site and explore our interactive map to understand
-                    the effects of lead pipe continue to have.
-                  </h1>
-              </div>
+          <div topmargin="20px">
+            <h1 className="title" style={{fontWeight: 'bold'}}>
+              How can you get involved?
+            </h1>
           </div>
-          <img  alt="Working man" src={WorkingMan} height={200} width={220}/>
-        </div>
-        <div>
-          <Button variant="primary" size="lg" onClick={() => navigate("/howtocheckpipes")}>
-             How to check my pipes
-          </Button>
-        </div>
+
+          <div>
+            <h1 className="supporting-text">
+              {" "}
+              Get involved to help your community progress towards a lead-free
+              future.
+            </h1>
+          </div>
+
+        <br />
+
+          <div>
+            <div className="numbered-sections">
+                <div className="item-1">
+                  <img
+                    alt="Number one"
+                    src={One}
+                    height={50}
+                    width={50}
+                  />
+                </div>
+                <div className="item-2">
+                    <h1 className="feature-text-top">
+                      {" "}
+                      Stay up-to-date on the impactlead pipes have in your community.
+                    </h1>
+                    <h1 className="feature-text-bottom">
+                      Read through this site and explore our interactive map to understand
+                      the effects of lead pipe continue to have.
+                    </h1>
+                </div>
+            </div>
+
+            <div>
+              <img  alt="Working man" src={WorkingMan} height={200} width={220}/>
+            </div>
+
+            <div>
+              <Button variant="primary" size="lg" onClick={() => navigate("/howtocheckpipes")}>
+                How to check my pipes
+              </Button>
+            </div>
+          </div>
 
         <br />
 
         <div>
-
           <div className="numbered-sections">
               <div className="item-1">
                 <img
@@ -75,7 +78,7 @@ const GetInvolved = () => {
               <div className="item-2">
                 <h1 className="feature-text-top">
                   {" "}
-                  Upload pictures of your water pipes to our platform.{" "}
+                  Upload pictures of your water pipes to our platform.
                 </h1>
                 <h1 className="feature-text-bottom">
                   This allows us to update the map on the latest data, which in turn
@@ -83,52 +86,53 @@ const GetInvolved = () => {
                 </h1>
               </div>
           </div>
-
           <br/>
-          <img
-            alt="man with camera cartoon"
-            src={CameraMan}
-            height={250}
-            width={260}
-          />
-        </div>
-        <div>
-          <Button variant="primary" size="lg" onClick={() => navigate("/contribute")} block>
-            Put your pipes on the map
-          </Button>
+            <div>
+              <img
+                alt="man with camera cartoon"
+                src={CameraMan}
+                height={250}
+                width={260}
+              />
+            </div>
+
+            <div>
+              <Button variant="primary" size="lg" onClick={() => navigate("/contribute")} block>
+                Put your pipes on the map
+              </Button>
+            </div>
         </div>
 
         <br />
 
         <div>
-
-
-        <div className="numbered-sections">
-          <div className="item-1">
-          <img
-              alt="Number three"
-              src={Three}
-              height={50}
-              width={50}
-              />
+          <div className="numbered-sections">
+              <div className="item-1">
+                <img
+                    alt="Number three"
+                    src={Three}
+                    height={50}
+                    width={50}
+                    />
+              </div>
+              <div className="item-2">
+                <h1 className="feature-text-top">
+                {" "}
+                  Share our interactive map to people in your community!
+                 </h1>
+                  <h1 className="feature-text-bottom">
+                  We can only have a farther reach and impact with your help.
+                  </h1>
+              </div>
+            <div>
+                <img
+                  alt="two people"
+                  src={TwoPeople}
+                  height={220}
+                  width={240}
+                />
+            </div>
           </div>
-          <div className="item-2">
-            <h1 className="feature-text-top">
-              {" "}
-              Share our interactive map to people in your community!
-            </h1>
-            <h1 className="feature-text-bottom">
-              We can only have a farther reach and impact with your help.
-            </h1>
-          </div>
-        </div>
-
-          <img
-            alt="two people"
-            src={TwoPeople}
-            height={220}
-            width={240}
-          />
         </div>
 
         <br />
