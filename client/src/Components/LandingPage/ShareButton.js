c7b44d7fdf4ed8d49fcd376a2099607c116fb485
“Alex <alexandriadelaney923@gmail.com>
2022-08-31 17:37:27 -0700

    changed social share to be inline instead of sticky

--- client/src/Components/LandingPage/ShareButton.js
+++ client/src/Components/LandingPage/ShareButton.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { StickyShareButtons } from "sharethis-reactjs";
-
+import { InlineShareButtons } from "sharethis-reactjs";
 
 const ShareButton = () => {
     return (
       <div className="sharingButtons">
-        <StickyShareButtons
+        <InlineShareButtons
           config={{
-            alignment: "left", // alignment of buttons (left, right)
+            alignment: "center", // alignment of buttons (left, center, right)
             color: "social", // set the color of buttons (social, white)
             enabled: true, // show/hide buttons (true, false)
             font_size: 16, // font size for the buttons
-            hide_desktop: false, // hide buttons on desktop (true, false)
-            labels: "counts", // button labels (cta, counts, null)
+            // labels: "cta", // button labels (cta, counts, null)
             language: "en", // which language to use (see LANGUAGES)
-            min_count: 100, // hide react counts less than min_count (INTEGER)
             networks: [
               // which networks to include (see SHARING NETWORKS)
               "twitter",
@@ -23,14 +20,11 @@ const ShareButton = () => {
             ],
             padding: 12, // padding within buttons (INTEGER)
             radius: 4, // the corner radius on each button (INTEGER)
-            show_total: true, // show/hide the total share count (true, false)
-            show_mobile: true, // show/hide the buttons on mobile (true, false)
-            show_toggle: true, // show/hide the toggle buttons (true, false)
-            size: 48, // the size of each button (INTEGER)
-            top: 160, // offset in pixels from the top of the page
+            show_total: true,
+            size: 40, // the size of each button (INTEGER)
 
             // OPTIONAL PARAMETERS
-            url: "https://github.com/LawrenceOL/productclubteam3", // (defaults to current url)
+            url: "https://www.sharethis.com", // (defaults to current url)
             image: "https://bit.ly/2CMhCMC", // (defaults to og:image or twitter:image)
             description: "custom text", // (defaults to og:description or twitter:description)
             title: "custom title", // (defaults to og:title or twitter:title)
